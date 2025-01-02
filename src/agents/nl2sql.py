0bd57d1855ce33c50fd92e0600528e23ba2f7200
Amit Singh <one.amitsingh@gmail.com>
2025-01-02 12:20:42 +0530

    prompt fetch restructure

--- src/agents/nl2sql.py
+++ src/agents/nl2sql.py
@@ -1,7 +1,5 @@
 """Module providing a langchain agent to answer user queries."""
 from langchain.agents import AgentExecutor, create_openai_functions_agent
-from langchain.agents import OpenAIFunctionsAgent
-from langchain.schema import SystemMessage
 # from langchain_community.chat_message_histories import RedisChatMessageHistory
 from langchain.prompts import MessagesPlaceholder
 
@@ -11,28 +9,14 @@ from src.llm.llm_provider import LLMProvider
 from src.db_connector.sql_server import get_db
 from langchain_community.agent_toolkits import SQLDatabaseToolkit
 
-from langchain_community.vectorstores import FAISS
-from langchain_core.example_selectors import SemanticSimilarityExampleSelector
-from langchain_openai import OpenAIEmbeddings
-
-from langchain_core.prompts import PromptTemplate
-from langchain_core.prompts import FewShotPromptTemplate
-from src.data.examples import query_examples
-
 from langchain_core.prompts import ChatPromptTemplate, SystemMessagePromptTemplate, MessagesPlaceholder
 
 def get_nl2sql_agent():
-    nl2sql_prompt = get_nl2sql_prompt()
-    
-    if len(query_examples) > 0:
-        few_shot_prompt = get_few_shot_prompt(nl2sql_prompt)
-        final_prompt = SystemMessagePromptTemplate(prompt=few_shot_prompt)
-    else:
-        final_prompt = ("system", nl2sql_prompt)
+    nl2sql_system_prompt = get_nl2sql_prompt()
 
     prompt = ChatPromptTemplate.from_messages(
         [
-            final_prompt,
+            nl2sql_system_prompt,
             ("human", "{input}"),
             MessagesPlaceholder("agent_scratchpad")
         ]
@@ -48,26 +32,3 @@ def get_nl2sql_agent():
     
     agent = create_openai_functions_agent(llm, tools, prompt)
     return AgentExecutor(agent=agent, tools=tools, verbose=True, max_iterations=10, max_execution_time=50, stream_runnable=False)
-
-def get_few_shot_prompt(system_prompt) -> FewShotPromptTemplate:
-    
-    embeddings = LLMProvider.get_embedding_model()
-    example_selector = SemanticSimilarityExampleSelector.from_examples(
-        query_examples,
-        embeddings,
-        FAISS,
-        k=5,
-        input_keys=["input"]
-    )
-
-    example_prompt = PromptTemplate.from_template("Question: {input}\n SQL Query: {sql_query}\n")
-
-    system_prompt = system_prompt + "\n##Query Examples:\n"
-    
-    return FewShotPromptTemplate(
-        example_selector=example_selector,
-        example_prompt=example_prompt,
-        prefix=system_prompt,
-        suffix="",
-        input_variables=["input"],
-    )
--- src/prompts/nl2sql_system_prompt.py
+++ src/prompts/nl2sql_system_prompt.py
@@ -1,6 +1,45 @@
 from langchain import hub
 
+from langchain_community.vectorstores import FAISS
+from langchain_core.example_selectors import SemanticSimilarityExampleSelector
+from src.llm.llm_provider import LLMProvider
+
+from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import FewShotPromptTemplate
+from src.data.examples import query_examples
+from langchain_core.prompts import SystemMessagePromptTemplate
+
 def get_nl2sql_prompt():
     prompt_template = hub.pull("langchain-ai/sql-agent-system-prompt")
     prompt_message = prompt_template.format(dialect="Microsoft SQL Server (mssql)", top_k=5)
-    return prompt_message
+    
+    if len(query_examples) > 0:
+        few_shot_prompt = get_few_shot_prompt(prompt_message)
+        final_prompt = SystemMessagePromptTemplate(prompt=few_shot_prompt)
+    else:
+        final_prompt = ("system", prompt_message)
+
+    return final_prompt
+
+def get_few_shot_prompt(system_prompt) -> FewShotPromptTemplate:
+    
+    embeddings = LLMProvider.get_embedding_model()
+    example_selector = SemanticSimilarityExampleSelector.from_examples(
+        query_examples,
+        embeddings,
+        FAISS,
+        k=5,
+        input_keys=["input"]
+    )
+
+    example_prompt = PromptTemplate.from_template("Question: {input}\n SQL Query: {sql_query}\n")
+
+    system_prompt = system_prompt + "\n##Query Examples:\n"
+    
+    return FewShotPromptTemplate(
+        example_selector=example_selector,
+        example_prompt=example_prompt,
+        prefix=system_prompt,
+        suffix="",
+        input_variables=["input"],
+    )
